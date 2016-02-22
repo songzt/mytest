36c41ef25ee92ca5de41915f82990ac79204d5f7
tarena-10 <tarena-10@tarena-10>
2016-02-22 11:49:05 +0800

    删了

--- Demo/src/com/songzt/test/Demo1.java
+++ Demo/src/com/songzt/test/Demo1.java
@@ -4,9 +4,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		//呵呵呵
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		
 		System.out.println(11);
 		
 		System.out.println(22);
